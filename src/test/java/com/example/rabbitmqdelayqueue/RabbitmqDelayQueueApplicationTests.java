5dfe80603cec0eac2c79c5f640e012a3b0a42fa6
park <park@parkuiMcBookPro.kornet>
2023-12-04 22:22:22 +0900

    Initial commit

---
+++ src/test/java/com/example/rabbitmqdelayqueue/RabbitmqDelayQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqdelayqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDelayQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
